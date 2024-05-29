80eadeed4e257ef275476bf23d5f4c161119829b
ablake12 <ablake12@scmail.spelman.edu>
2024-05-29 02:04:21 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,10 @@
 from datetime import datetime
-from summary import Summarization
+import tkinter as tk
+from summary_extract.summary_gui import summaryGUI
 
 if __name__ == '__main__':
     start_time = datetime.now()
-    doc_option = int(input("1. Pdf\n2. Word Document\n3. Article URL\nPlease enter a document type to summarize: "))
-    summary = Summarization(doc_option)
-    if doc_option == 1:
-        summary.get_pdf_content()
-    elif doc_option == 2:
-        summary.get_docx_content()
-    elif doc_option == 3:
-        summary.get_url_content()
-    summary_option = int(input("1. Extractive\n2. Abstractive\nPlease choose the type of summarization: "))
-    if summary_option == 1:
-        summary.extractive_summarize()
-    elif summary_option == 2:
-        summary.abstractive_summarize()
-
+    window = tk.Tk()
+    app = summaryGUI(window)
+    window.mainloop()
     print(f"Runtime of the program: {datetime.now() - start_time}")
