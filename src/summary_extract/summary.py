03d484a4b1c5e58da61225419f47a3d89cf1f79f
ablake12 <ablake12@scmail.spelman.edu>
2025-01-30 00:58:41 -0500

    Add percentage to number of tokens

--- src/summary_extract/summary.py
+++ src/summary_extract/summary.py
@@ -52,7 +52,7 @@ class Summarization:
                 self.file_name = "docs/{:<0.50}_summary.txt".format(summary_title)
                 for paragraph in soup.find_all('p'):
                     text = " ".join(paragraph.text.split())
-                    sentences = self.spacy_tokenizer(text)
+                    sentences = self.spacy_tokenizer(text) # use spacy tokenizer to split text into sentences
                     for sent in sentences:
                         self.content += sent.text + "\n"
                 content_msg = f"Content extracted successfully.\nRuntime of getting the content: {datetime.now() - start_time}"
@@ -69,9 +69,9 @@ class Summarization:
             start_time = datetime.now()
             doc = docx.Document(self.docx)
             for paragraph in doc.paragraphs:
-                if paragraph.text.strip():
-                    text = " ".join(paragraph.text.split())
-                    sentences = self.spacy_tokenizer(text)
+                if paragraph.text.strip(): # strip white spaces
+                    text = " ".join(paragraph.text.split()) # cleaning up additional white spaces
+                    sentences = self.spacy_tokenizer(text) # use spacy tokenizer to split text into sentences
                     for sent in sentences:
                         self.content += sent.text + "\n"
             content_msg = f"Content extracted successfully.\nRuntime of getting the content: {datetime.now() - start_time}"
@@ -82,7 +82,7 @@ class Summarization:
     def is_pdf_encrypted(self): # function that checks for an encrypted file
         try:
             self.pdf_reader = PyPDF2.PdfReader(self.pdf)
-            if self.pdf_reader.is_encrypted:
+            if self.pdf_reader.is_encrypted: # check to see if user is trying to access encrypted pdf
                 return True
             else:
                 return False
@@ -91,10 +91,10 @@ class Summarization:
             return error_msg
     def decrypt_pdf(self, password): # function to return whether the password is correct or not
         try:
-            if self.pdf_reader.decrypt(password):
+            if self.pdf_reader.decrypt(password): # check if password is correct
                 return True
             else:
-                return False
+                return False # password is incorrect
         except Exception as error:
             error_msg = f"Error encrypting pdf document: {error}"
             return error_msg
@@ -103,9 +103,9 @@ class Summarization:
             start_time = datetime.now()
             for page_num in range(len(self.pdf_reader.pages)):
                 page = self.pdf_reader.pages[page_num]
-                if page.extract_text().strip():
-                    text = " ".join(page.extract_text().split())
-                    sentences = self.spacy_tokenizer(text)
+                if page.extract_text().strip(): # strip white spaces
+                    text = " ".join(page.extract_text().split()) # cleaning up additional white spaces
+                    sentences = self.spacy_tokenizer(text) # use spacy tokenizer to split text into sentences
                     for sent in sentences:
                         self.content += sent.text + "\n"
             content_msg = f"Content extracted successfully.\nRuntime of getting the content: {datetime.now() - start_time}"
@@ -116,60 +116,65 @@ class Summarization:
     def extractive_summarize(self): # extractive model that creates a summary that highlights the most important parts of the content
         try:
             percentage = 0.25
-            stopwords = list(STOP_WORDS)
-            doc = self.nlp(self.content)
+            stopwords = list(STOP_WORDS) # small words to be filtered out
+            doc = self.nlp(self.content) # tokenizing the content
 
-            tokens = [token.text for token in doc] # tokenizing the content
-            word_frequencies = {}
+            word_frequencies = {} # dictionary to store word frequencies
 
             for word in doc: # the frequency of each word is calculated 
                 if word.text.lower() not in stopwords:
                     if word.text.lower() not in punctuation:
                         if word.text not in word_frequencies.keys():
-                            word_frequencies[word.text] = 1
+                            word_frequencies[word.text] = 1 # count word for the first time if it's not a stop word or a punctuation
                         else:
-                            word_frequencies[word.text] += 1
+                            word_frequencies[word.text] += 1 # continue to count word that's already in the dictionary
             maximum_frequency = max(word_frequencies.values())
             for word in word_frequencies.keys():
-                word_frequencies[word] = (word_frequencies[word]/maximum_frequency)
+                word_frequencies[word] = (word_frequencies[word]/maximum_frequency) # divide frequencies by the word with the highest frequency to get a frequency between 0 to 1
 
-            sent_list = [sent for sent in doc.sents]
-            sent_scores = {}
+            # sentence scores are calculated based on word frequencies to determine most important sentences for the summary
+            sent_list = [sent for sent in doc.sents] # sentences in the document
+            sent_scores = {} # dictionary for the scores
             for sent in sent_list: # sentences are being scored based on the word frequencies
-                for word in sent:
-                    if word.text.lower() in word_frequencies.keys():
+                for word in sent: # go through each word in sentence
+                    if word.text.lower() in word_frequencies.keys(): # if the word is important then we'll get its frequency and add it to the sentence score
                         if sent not in sent_scores.keys():                            
-                            sent_scores[sent]=word_frequencies[word.text.lower()]
+                            sent_scores[sent]=word_frequencies[word.text.lower()] # if it's the first time adding for that particular sentence
                         else:
-                            sent_scores[sent]+=word_frequencies[word.text.lower()]
-            len_tokens=int(len(sent_list)*percentage)
+                            sent_scores[sent]+=word_frequencies[word.text.lower()] # if it's not the first time adding for that particular sentence
+            len_tokens=int(len(sent_list)*percentage) # total amount of tokens for the summary (25% of original content)
+
             # Summary for the sentences with maximum score. Here, each sentence in the list is of spacy.span type
-            summary = nlargest(n = len_tokens, iterable = sent_scores,key=sent_scores.get)
-            final_summary=[word.text for word in summary]
-            with open(self.file_name, "w") as out_file:
+            summary = nlargest(n = len_tokens, iterable = sent_scores,key=sent_scores.get) # returns the sentences with the top score based on the maximum amount of sentences allowed
+            final_summary=[word.text for word in summary] # get text attribute from the final summary
+
+            with open(self.file_name, "w") as out_file: # write final summary to file
                 for sum in final_summary:
                     out_file.write(sum)
             summary_msg = f"Extractive summary extracted to {self.file_name}"
             print(summary_msg)
-            return (summary_msg, 0)
+            return (summary_msg, 0) # return success message and code
         except Exception as error:
             error_msg = f"Error with getting the extractive summary: {error}"
-            return (error_msg, 1)
+            return (error_msg, 1) # return failure message and code
     def summarize_content(self, content, out_file): # function to summarize content using the bart model
-        input_ids = self.bart_tokenizer.encode(content, return_tensors='pt')
-        summary_ids = self.bart_model.generate(input_ids, num_beams=4, max_length=250, early_stopping=True)
-        summary = self.bart_tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-        sentences = self.spacy_tokenizer(summary)
+        max_percentage = 0.25
+        max_len = round(len(content) * max_percentage) # total amount of tokens for the summary (25% of original content)
+        input_ids = self.bart_tokenizer.encode(content, return_tensors='pt') # encodes into numerical input ids
+        summary_ids = self.bart_model.generate(input_ids, num_beams=4, max_length=max_len, early_stopping=True) # generates summary based on input ids, amount of beams to search for summary, max length of tokens
+        summary = self.bart_tokenizer.decode(summary_ids[0], skip_special_tokens=True) # decodes back into alphabetical summary
+        sentences = self.spacy_tokenizer(summary) # tokenizers to split into sentences
         summary_text = "\n".join([sent.text for sent in sentences])
-        out_file.write(summary_text + "\n")
-        out_file.flush()
+        out_file.write(summary_text + "\n") # write to outfile
+        out_file.flush() # saves summary to file quickly in case of file closure
+
     def chunk_split(self, max_length):
-        sentences = self.spacy_tokenizer(self.content)
+        sentences = self.spacy_tokenizer(self.content) # use spacy tokenizer for getting sentences
         chunks = []
         current_chunk = ""
 
         for sent in sentences:
-            if len(current_chunk) + len(sent.text) + 1 > max_length:
+            if len(current_chunk) + len(sent.text) + 1 > max_length: # the current chunk length + the incoming sentence length + 1 to account for spacing between the sentences
                 # if the sentences and the current chunk is more than 1024 tokens then a new chunk has to be created and the current chunk is added to the chunk list
                 chunks.append(current_chunk.strip())
                 current_chunk = sent.text + " "
@@ -177,7 +182,7 @@ class Summarization:
                 # the sentence gets added to the current chunk if it doesn't exceed the max length of tokens
                 current_chunk += sent.text + " "
 
-        if current_chunk: # the most recent chunk is being appended
+        if current_chunk: # the most recent chunk after iterating is being appended
             chunks.append(current_chunk.strip())
 
         return chunks
@@ -186,14 +191,14 @@ class Summarization:
             chunk_size = 1024 # max tokens the model allows
             if len(self.content) > chunk_size: # if the content is more than the model allows it has to be chunked
                 chunks = self.chunk_split(chunk_size) # split into chunks based on sentences
-                max_threads = mp.cpu_count()
+                max_threads = mp.cpu_count() # using the max cpus for workers
                 with open(self.file_name, "w") as out_file:
                     with ThreadPoolExecutor(max_workers=max_threads) as executor: # chunks are summarized in parallel
                         retrieval_tasks = []
                         for i in range(len(chunks)):
                             task = executor.submit(self.summarize_content, chunks[i], out_file)
                             retrieval_tasks.append(task)
-                    concurrent.futures.wait(retrieval_tasks)
+                    concurrent.futures.wait(retrieval_tasks) # waits for all the tasks to be completed
             else: # if the content is less than 1024 tokens it can just be summarized and written to the file
                 with open(self.file_name, "w") as out_file:
                     self.summarize_content(self.content, out_file)
